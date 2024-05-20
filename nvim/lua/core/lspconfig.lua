e4f7b06a5c6241dc67ab3643026d99be8eb9ee16
LeonardoNatale <natale.leonardo1997@gmail.com>
2024-05-20 23:25:02 +0200

    add pyright for python autocompletion

--- nvim/lua/core/lspconfig.lua
+++ nvim/lua/core/lspconfig.lua
@@ -124,6 +124,20 @@ return {
       tailwindcss = {},
       cssls = {},
       ruff_lsp = {},
+      pyright = {
+        settings = {
+          pyright = {
+            -- Using Ruff's import organizer
+            disableOrganizeImports = true,
+          },
+          python = {
+            analysis = {
+              -- Ignore all files for analysis to exclusively use Ruff for linting
+              ignore = { '*' },
+            },
+          },
+        },
+      },
       yamlls = {},
       lua_ls = {
         -- cmd = {...},
