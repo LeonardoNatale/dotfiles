55a80428eab5fa7f02183669a7f7f73c4ac49842
LeonardoNatale <natale.leonardo1997@gmail.com>
2024-10-06 23:19:34 +0200

    fix gitlinker mapping to avoid conflict

--- nvim/lua/custom/gitlinker.lua
+++ nvim/lua/custom/gitlinker.lua
@@ -3,8 +3,8 @@ return {
   cmd = "GitLink",
   opts = {},
   keys = {
-    { "<leader>gm", "<cmd>GitLink default_branch<cr>",  mode = { "n", "v" }, desc = "[G]it [M]ain link" },
-    { "<leader>gc", "<cmd>GitLink current_branch<cr>",  mode = { "n", "v" }, desc = "[G]it [C]urrent [B]ranch link" },
+    { "<leader>glm", "<cmd>GitLink default_branch<cr>",  mode = { "n", "v" }, desc = "[G]it [L]ink main" },
+    { "<leader>glb", "<cmd>GitLink current_branch<cr>",  mode = { "n", "v" }, desc = "[G]it [L]ink branch" },
     -- { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git " },
   },
 }
