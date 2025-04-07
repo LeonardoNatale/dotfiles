1f90158afe2f290f44c124624084727a4930fd86
LeonardoNatale <natale.leonardo1997@gmail.com>
2025-04-07 08:28:23 +0200

    add fish install

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 ln -s ~/dev/dotfiles/kitty ~/.config/kitty
 ln -s ~/dev/dotfiles/nvim ~/.config/nvim
 ln -s ~/dev/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dev/dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -s ~/dev/dotfiles/fish/fish_plugins ~/.config/fish/fish_plugins
 ln -s ~/dev/dotfiles/starship/starship.toml ~/.config/starship.toml
 
