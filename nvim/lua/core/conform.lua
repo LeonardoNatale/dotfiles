d346aa1ee5fb4737a52a64d86ce097633dfbd01e
LeonardoNatale <natale.leonardo1997@gmail.com>
2025-07-29 16:22:15 +0200

    update

--- nvim/lua/core/conform.lua
+++ nvim/lua/core/conform.lua
@@ -1,21 +1,21 @@
 return {
   'stevearc/conform.nvim',
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
   opts = {
     default_format_opts = {
-      lsp_format = "fallback", -- use LSP formatting if available, fallback to formatters defined below
+      lsp_format = 'fallback', -- use LSP formatting if available, fallback to formatters defined below
     },
     formatters_by_ft = {
-      lua = { "stylua" },
-      python = { "ruff_fix", "ruff_format" },
-      javascript = { "biome", "biome-check", "biome-organize-imports", "eslint_d" },
-      javascriptreact = { "biome", "biome-check", "biome-organize-imports", "eslint_d" },
-      typescript = { "biome", "biome-check", "biome-organize-imports", "eslint_d" },
-      typescriptreact = { "biome", "biome-check", "biome-organize-imports", "eslint_d" },
-      json = { "biome", "biome-check" },
-      sql = { "sqruff" },
-      ["sql.jinja"] = { "sqruff" },
+      lua = { 'stylua' },
+      python = { 'ruff_fix', 'ruff_format' },
+      javascript = { 'biome', 'biome-check', 'biome-organize-imports', 'eslint_d' },
+      javascriptreact = { 'biome', 'biome-check', 'biome-organize-imports', 'eslint_d' },
+      typescript = { 'biome', 'biome-check', 'biome-organize-imports', 'eslint_d' },
+      typescriptreact = { 'biome', 'biome-check', 'biome-organize-imports', 'eslint_d' },
+      json = { 'biome', 'biome-check' },
+      sql = { 'sqruff' },
+      ['sql.jinja'] = { 'sqruff' },
       terraform = { 'terraform_fmt' },
     },
     format_on_save = { timeout_ms = 500 },
