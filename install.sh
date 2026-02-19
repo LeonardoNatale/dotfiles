1bb05208db169d2158bc9bc41432272577b9b833
LeonardoNatale <natale.leonardo1997@gmail.com>
2026-02-19 16:02:51 +0100

    update instlal scripts

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ ln -s ~/dev/dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -s ~/dev/dotfiles/fish/fish_plugins ~/.config/fish/fish_plugins
 ln -s ~/dev/dotfiles/fish/functions ~/.config/fish/functions
 ln -s ~/dev/dotfiles/starship/starship.toml ~/.config/starship.toml
+ln -s ~/dev/dotfiles/ghostty ~/.config/ghostty
 
